36d19ad52d5a47091af3cd9e776636384912d6c0
racross1 <racross1@gmail.com>
2020-11-29 16:40:42 -0800

    refactor

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,21 @@
+
+module Memorable
+    
+    module ClassMethods
+
+        def reset_all
+            all.clear
+        end
+
+        def count
+            all.count
+        end
+    end 
+
+end
+
+
+
+
+
+
